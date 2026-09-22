5c261cb6b2c6ddc6893569d6b13327fdb9751abc
vinuk7423-blip <vinuk7423@gmail.com>
2026-09-22 15:25:02 +0530

    loops

---
+++ Datatypes/example.py
@@ -0,0 +1,3 @@
+print("Hello World")
+
+#this is the first program
---
+++ Loops/For/keyvalue_dictionary.py
@@ -0,0 +1,7 @@
+employee = {
+    "name": "Vismitha",
+    "age": 25,
+    "salary": 25000
+}
+for key,value in employee.items():
+    print(key,value)
